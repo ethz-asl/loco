7e9e3609ec08059bda1cf1dc76149049fdddd41f
Christian Gehring <gehrinch@ethz.ch>
2014-02-21 17:06:34 +0100

    Added GaitPatternAPSTest

---
+++ gait_pattern/test/GaitPatternAPSTest.cpp
@@ -0,0 +1,15 @@
+/*
+ * GaitPatternAPSTest.cpp
+ *
+ *  Created on: Feb 21, 2014
+ *      Author: gech
+ */
+
+
+
+#include "APS.hpp"
+#include <gtest/gtest.h>
+
+TEST(GaitPatternAPSTest, testAPS) {
+  loco::APS aps;
+}
