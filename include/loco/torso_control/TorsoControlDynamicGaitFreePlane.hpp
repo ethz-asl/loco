f855535d1399ad5f1acedeb96e3ead1c943bb27a
dbellicoso <bellicosodario@gmail.com>
2014-09-18 16:36:45 +0200

    Merged with free plane foot strategy. TorsoControllerFreePlane now inherits directly from TorsoControlerDynamicGait.

--- include/loco/torso_control/TorsoControlDynamicGaitFreePlane.hpp
+++ include/loco/torso_control/TorsoControlDynamicGaitFreePlane.hpp
@@ -9,21 +9,10 @@
 #define LOCO_TORSOCONTROLDYNAMICGAITFREEPLANE_HPP_
 
 
-#include "loco/torso_control/TorsoControlBase.hpp"
-#include "loco/torso_control/CoMOverSupportPolygonControl.hpp"
-
-#include "loco/common/LegGroup.hpp"
-#include "loco/common/TorsoBase.hpp"
-
-#include "kindr/rotations/RotationEigen.hpp"
-
-#include "loco/common/TerrainModelBase.hpp"
-
-#include "PeriodicRBF1DC1.hpp"
-
+#include "loco/torso_control/TorsoControlDynamicGait.hpp"
 namespace loco {
 
-  class TorsoControlDynamicGaitFreePlane: public TorsoControlBase {
+  class TorsoControlDynamicGaitFreePlane: public TorsoControlDynamicGait {
    public:
     enum AdaptToTerrain {CompleteAdaption, SaturatedLinearAdaption};
     TorsoControlDynamicGaitFreePlane(LegGroup* legs, TorsoBase* torso, loco::TerrainModelBase* terrain);
@@ -32,42 +21,10 @@ namespace loco {
     virtual bool initialize(double dt);
     virtual bool advance(double dt);
 
-    virtual RotationQuaternion computeHeading(const RotationQuaternion& rquat, const Vector& axis);
-    RotationQuaternion decomposeRotation(const RotationQuaternion& q_BA, const Vector& vB);
-    CoMOverSupportPolygonControl* getCoMControl();
-    const CoMOverSupportPolygonControl& getCoMControl() const;
-    virtual bool loadParameters(const TiXmlHandle& handle);
+   protected:
 
-    double getDesiredTorsoForeHeightAboveGroundInWorldFrameOffset() const;
-    double getDesiredTorsoHindHeightAboveGroundInWorldFrameOffset() const;
-
-    void setDesiredPositionOffetInWorldFrame(const Position& positionOffsetInWorldFrame);
-    void setDesiredOrientationOffset(const RotationQuaternion& orientationOffset);
-
-    /*! Computes an interpolated version of the two controllers passed in as parameters.
-     *  if t is 0, the current setting is set to controller1, 1 -> controller2, and values in between
-     *  correspond to interpolated parameter set.
-     * @param torsoController1
-     * @param torsoController2
-     * @param t interpolation parameter
-     * @returns true if successful
-     */
-    virtual bool setToInterpolated(const TorsoControlBase& torsoController1, const TorsoControlBase& torsoController2, double t);
-
-   private:
-    LegGroup* legs_;
-    TorsoBase* torso_;
-    loco::TerrainModelBase* terrain_;
-    CoMOverSupportPolygonControl comControl_;
-
-    double headingDistanceFromForeToHindInBaseFrame_;
-    double desiredTorsoForeHeightAboveGroundInWorldFrameOffset_;
-    double desiredTorsoHindHeightAboveGroundInWorldFrameOffset_;
     double desiredTorsoCoMHeightAboveGroundInWorldFrameOffset_;
-    rbf::PeriodicRBF1DC1 desiredTorsoForeHeightAboveGroundInWorldFrame_;
-    rbf::PeriodicRBF1DC1 desiredTorsoHindHeightAboveGroundInWorldFrame_;
-    Position desiredPositionOffetInWorldFrame_;
-    RotationQuaternion desiredOrientationOffset_;
+
     double maxDesiredPitchRadians_;
     double desiredPitchSlope_;
     double maxDesiredRollRadians_;
@@ -76,10 +33,7 @@ namespace loco {
 
     template <typename T> int sgn(T val);
 
-   private:
-    virtual bool loadParametersHipConfiguration(const TiXmlHandle &hParameterSet);
-    virtual bool loadHeightTrajectory(const TiXmlHandle &hTrajectory,  rbf::PeriodicRBF1DC1& trajectory);
-    bool interpolateHeightTrajectory(rbf::PeriodicRBF1DC1& interpolatedTrajectory, const rbf::PeriodicRBF1DC1& trajectory1, const rbf::PeriodicRBF1DC1& trajectory2, double t);
+   protected:
     void getDesiredBasePitchFromTerrainPitch(const double terrainPitch, double& desiredBasePitch);
     void getDesiredBaseRollFromTerrainRoll(const double terrainRoll, double& desiredBaseRoll);
 
