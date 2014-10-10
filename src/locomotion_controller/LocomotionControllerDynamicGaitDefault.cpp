f684394f1494ff0a615bfabfe9fc71c1cfa37d4c
dbellicoso <bellicosodario@gmail.com>
2014-10-10 15:26:30 +0200

    Use foot placement inverted pendulum in default loco controller.

--- src/locomotion_controller/LocomotionControllerDynamicGaitDefault.cpp
+++ src/locomotion_controller/LocomotionControllerDynamicGaitDefault.cpp
@@ -52,7 +52,7 @@ LocomotionControllerDynamicGaitDefault::LocomotionControllerDynamicGaitDefault(c
     gaitPatternAPS_.reset(new loco::GaitPatternAPS);
     gaitPatternFlightPhases_.reset(new loco::GaitPatternFlightPhases);
     limbCoordinator_.reset(new loco::LimbCoordinatorDynamicGait(legs_.get(), torso_.get(), gaitPatternFlightPhases_.get()));
-    footPlacementStrategy_.reset(new loco::FootPlacementStrategyFreePlane(legs_.get(), torso_.get(), terrainModel_.get()));
+    footPlacementStrategy_.reset(new loco::FootPlacementStrategyInvertedPendulum(legs_.get(), torso_.get(), terrainModel_.get()));
     torsoController_.reset(new loco::TorsoControlDynamicGaitFreePlane(legs_.get(), torso_.get(), terrainModel_.get()));
     contactForceDistribution_.reset(new loco::ContactForceDistribution(torso_, legs_, terrainModel_));
     virtualModelController_.reset(new loco::VirtualModelController(legs_, torso_, contactForceDistribution_));
