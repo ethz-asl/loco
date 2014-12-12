84025f8e94ac98b0911a4edb2a8d506532829132
Dario Bellicoso <bellicosodario@gmail.com>
2014-12-12 02:05:27 +0100

    Update StateSwitcher.hpp

--- include/loco/state_switcher/StateSwitcher.hpp
+++ include/loco/state_switcher/StateSwitcher.hpp
@@ -1,7 +1,7 @@
 /*******************************************************************************************
 * Software License Agreement (BSD License)
 *
-* Copyright (c) 2014, Christian Gehring, Péter Fankhauser, C. Dario Bellicoso, Stelian Coros
+* Copyright (c) 2014, C. Dario Bellicoso, Christian Gehring, Péter Fankhauser, Stelian Coros
 * All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
@@ -36,7 +36,7 @@
  * StateSwitcher.hpp
  *
  *  Created on: Oct 5, 2014
- *      Author: dario
+ *      Author: C. Dario Bellicoso
  */
 
 #ifndef LOCO_STATESWITCHER_HPP_
